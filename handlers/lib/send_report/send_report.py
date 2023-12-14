2e4384570fffe0f2fe1b2d721e8d0a7435664787
polkadot21 <saurov.ev@gmail.com>
2023-12-15 01:49:55 +0300

    implement send report

--- handlers/lib/send_report/send_report.py
+++ handlers/lib/send_report/send_report.py
@@ -1,6 +1,5 @@
 import json
-from typing import Dict, Any, Union
-
+from typing import Dict, Any, Union, List
 import requests
 from telegram import Update
 from telegram.ext import (
@@ -11,7 +10,7 @@ from telegram.ext import (
     Filters,
 )
 from typeguard import typechecked
-from utils import Config, format_report_with_gpt, ReportWithMetadata, Prompts
+from utils import Config, format_report_with_gpt, ReportWithMetadata, Prompts, MetaData
 from requests import Response
 from logging import Logger
 from ..send_menu import send_menu_handler_factory
@@ -23,16 +22,17 @@ CLIENT_REPORT = 0
 def send_report_handler_factory(
     cfg: Config, prompts: Prompts, logger: Logger, messages: Dict[str, Any]
 ):
+    @typechecked
     def send_report(update: Update, context: CallbackContext) -> int:
         if update.effective_chat is None:
             raise ValueError
-        user_chat_id = str(update.effective_chat.id)
         context.bot.send_message(
             chat_id=update.effective_chat.id,
             text="Пожалуйста, напишите свой отчет о тренировке:",
         )
         return CLIENT_REPORT
 
+    @typechecked
     def receive_and_load_report(
         update: Update, context: CallbackContext
     ) -> Union[int, None]:
@@ -40,58 +40,133 @@ def send_report_handler_factory(
             raise TypeError
         user_chat_id = str(update.effective_chat.id)
 
-        current_year: int = 2023
-        current_week: int = 50
+        params = {
+            "tg_id": user_chat_id,
+        }
 
-        # get user's input (string)
-        client_response: Union[str, None] = update.message.text
-        if update.message.text is None:
-            raise ValueError
-        elif isinstance(client_response, str):
-            client_report: str = client_response
-
-        report: ReportWithMetadata = format_report_with_gpt(
-            cfg=cfg,
-            prompts=prompts,
-            tg_id=int(user_chat_id),
-            year=current_year,
-            week=current_week,
-            client_report=client_report,
-        )
-
-        logger.debug(f"Report: {json.dumps(report.dict())}")
+        logger.info(f"User: {user_chat_id} requested current training plan idx\n")
 
         try:
-            # # Send request to backend
-            # response: Response = requests.post(
-            #     f"{cfg.BACKEND_API}/{cfg.VERSION}/{cfg.CLIENT_REPORT_ENDPOINT}",
-            #     timeout=10,
-            #     json=report.dict(),
-            # )
-            #
-            # logger.debug(
-            #     f"Received HTTP {response.status_code} from backend for training plan request."
-            # )
-            #
-            # # Raise exception for HTTP errors
-            # response.raise_for_status()
-
-            # response_dict: Dict[str, Any] = response.json()
-            # get year and week from response_dict
-            year = 2023
-            week = 50
-
-            context.bot.send_message(
-                chat_id=update.effective_chat.id,
-                text=messages["report_successfully_uploaded"].format(
-                    year=year, week=week
-                ),
+            # Send request to backend
+            get_response: Response = requests.get(
+                f"{cfg.BACKEND_API}/{cfg.VERSION}/{cfg.CURRENT_PERSONAL_TRAINING_ENDPOINT}",
+                params=params,
+                timeout=10,
             )
 
-            send_menu_handler = send_menu_handler_factory(cfg)
-            send_menu_handler(update, context)
+            logger.debug(
+                f"Received HTTP {get_response.status_code} from backend for metadata request."
+            )
 
-            return ConversationHandler.END
+            if get_response.status_code == 404:
+                context.bot.send_message(
+                    chat_id=update.effective_chat.id,
+                    text=messages["first_train_then_report"],
+                )
+                send_menu_handler = send_menu_handler_factory(cfg)
+                send_menu_handler(update, context)
+                return ConversationHandler.END
+            else:
+                get_response.raise_for_status()
+
+            data = get_response.json()
+
+            if isinstance(data, dict):
+                resources: List[Dict[str, Any]] = data["Resources"]
+                if not resources:  # If list is empty
+                    formatted_data = messages["no_current_personal_training"]
+                    logger.warning(
+                        f"No current training plans found for user {user_chat_id}."
+                    )
+                    context.bot.send_message(
+                        chat_id=update.effective_chat.id, text=formatted_data
+                    )
+                else:
+                    for resource in resources:
+                        if isinstance(resource, dict):
+                            metadata: MetaData = MetaData(**resource)
+                            client_response: Union[str, None] = update.message.text
+                            client_report: str = ""
+                            if update.message.text is None:
+                                raise ValueError
+                            elif isinstance(client_response, str):
+                                client_report: str = client_response
+
+                            report: ReportWithMetadata = format_report_with_gpt(
+                                cfg=cfg,
+                                prompts=prompts,
+                                metadata=metadata,
+                                client_report=client_report,
+                            )
+
+                            post_response: Response = requests.post(
+                                f"{cfg.BACKEND_API}/{cfg.VERSION}/{cfg.PERSONAL_TRAINING_REPORT}",
+                                timeout=10,
+                                json=report.dict(),
+                            )
+                            logger.debug(
+                                f"Received HTTP {post_response.status_code} from backend for post report request."
+                            )
+                            if post_response.status_code == 409:
+                                context.bot.send_message(
+                                    chat_id=update.effective_chat.id,
+                                    text=messages["report_already_exists"].format(
+                                        week=metadata.Week, year=metadata.Year
+                                    ),
+                                )
+                                send_menu_handler = send_menu_handler_factory(cfg)
+                                send_menu_handler(update, context)
+                                return ConversationHandler.END
+                            else:
+                                post_response.raise_for_status()
+
+                            logger.debug(f"Report: {json.dumps(report.dict())}")
+                            try:
+                                context.bot.send_message(
+                                    chat_id=update.effective_chat.id,
+                                    text=messages[
+                                        "report_successfully_uploaded"
+                                    ].format(year=metadata.Year, week=metadata.Week),
+                                )
+
+                                send_menu_handler = send_menu_handler_factory(cfg)
+                                send_menu_handler(update, context)
+                                return ConversationHandler.END
+
+                            except requests.Timeout:
+                                logger.warning(
+                                    f"Request to backend for training plan timed out for user {user_chat_id}."
+                                )
+                                context.bot.send_message(
+                                    chat_id=update.effective_chat.id,
+                                    text=messages["request_timeout"],
+                                )
+                            except (
+                                requests.RequestException
+                            ) as e:  # Catching other request-related exceptions
+                                logger.error(
+                                    f"Request to backend failed with error: {str(e)}"
+                                )
+                                context.bot.send_message(
+                                    chat_id=update.effective_chat.id,
+                                    text=messages["exception"].format(exception=str(e)),
+                                )
+                            except (
+                                ValueError
+                            ):  # This will catch non-JSON parsable responses
+                                logger.error(
+                                    f"Received non-JSON parsable response from backend for user {user_chat_id}."
+                                )
+                                context.bot.send_message(
+                                    chat_id=update.effective_chat.id,
+                                    text=messages["unexpected_data"],
+                                )
+
+                            send_menu_handler = send_menu_handler_factory(cfg)
+                            send_menu_handler(update, context)
+                            return None
+                        else:
+                            raise TypeError
 
         except requests.Timeout:
             logger.warning(
@@ -115,10 +190,6 @@ def send_report_handler_factory(
             context.bot.send_message(
                 chat_id=update.effective_chat.id, text=messages["unexpected_data"]
             )
-
-        send_menu_handler = send_menu_handler_factory(cfg)
-        send_menu_handler(update, context)
-        return None
 
     return send_report, receive_and_load_report
 
